19278a214dbc21f61070970877fdfe0095444add
Ev_aaron <121855959+Exaster@users.noreply.github.com>
2023-11-07 14:12:41 +0300

    Update localserver.py

--- web/localserver.py
+++ web/localserver.py
@@ -1,19 +1,11 @@
 import subprocess
 import webbrowser
 
-# Replace these variables with your actual repository path
+# Open current path
 repo_path = "path/to/your/repository"
 commit_message = "Automated commit"
 
-# Commit your changes
-try:
-    subprocess.run(["git", "add", "."], cwd=repo_path)
-    subprocess.run(["git", "commit", "-m", commit_message], cwd=repo_path)
-    print("Changes committed successfully.")
-except Exception as e:
-    print(f"Error committing changes: {e}")
-
-# Start a local web server
+# Start a local web server, default port 8000
 try:
     subprocess.run(["python", "-m", "http.server"], cwd=repo_path)
 except Exception as e:
